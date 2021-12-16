201766d8044629ccae6698566142b56f65edeabb
root <root@DESKTOP-2MDBIDV.localdomain>
2021-12-16 21:55:47 +0000

    Rat spawn set by Tiled map and using MatterJS physics implemented jumping

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,6 +4,9 @@ export default class Game extends Phaser.Scene{
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys; //! because in Phaser this will work fine
   
   private rat!: Phaser.Physics.Matter.Sprite;
+
+  private is_touching_ground = false;
+
   constructor(){
     super('game'); //identifier for extended class from Phase.Scene
   }
@@ -27,25 +30,45 @@ export default class Game extends Phaser.Scene{
     ground.setCollisionByProperty({collides:true}); // collides in the Tiled embbed tileset is a boolean that has been ticked for ground tiles
 
     this.matter.world.convertTilemapLayer(ground); // adds MatterTileBody instances for all collides tiles in the layer
+    
+    const objectsLayer = map.getObjectLayer('objects'); // getting the objects layer from tile map in Tiled
 
-    const {width,height} = this.scale;
-    this.rat = this.matter.add.sprite(width *0.5, height * 0.5, 'rat'); // adding the rat
-    this.rat.setFixedRotation();
+    objectsLayer.objects.forEach(objData => {
+      const {x = 0,y = 0,name, width = 0} = objData; // looping through object data and setting defaults for each
+      
+      switch (name){
+        case 'rat-spawn': // if object has name rat-spawn
+          this.rat = this.matter.add.sprite(x! + width * 0.5,y!, 'rat'); // adding the rat
+          this.rat.setFixedRotation(); // fix rotation of rat
+      
+          this.rat.setOnCollide((data: MatterJS.ICollisionPair) => {
+            this.is_touching_ground = true;
+          });
+      }
+
+    })
+    this.cameras.main.startFollow(this.rat);
   }
 
   update(){
     const speed = 10;
-      if (this.cursors.left.isDown){
-        this.rat.flipX = true;
-        this.rat.setVelocityX(-speed);
-        this.rat.play('player-walk', true);
-      }else if (this.cursors.right.isDown){
-        this.rat.flipX = false;
-        this.rat.setVelocityX(speed);
-        this.rat.play('player-walk', true);
-      }else{
-        this.rat.play('player-idle', true);
-      }
+    if (this.cursors.left.isDown){
+      this.rat.flipX = true;
+      this.rat.setVelocityX(-speed);
+      this.rat.play('player-walk', true);
+    }else if (this.cursors.right.isDown){
+      this.rat.flipX = false;
+      this.rat.setVelocityX(speed);
+      this.rat.play('player-walk', true);
+    }else{
+      this.rat.play('player-idle', true);
+    }
+
+    const space_pressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+    if (space_pressed && this.is_touching_ground){
+      this.rat.setVelocityY(-10);
+      this.is_touching_ground = false;
+    }
   }
 
   private createRatAnimations(){ // method for creating the rat animations including walking and idle
